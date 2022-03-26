910987f4a9c28d805640d7d519a2b15c307b4de4
AFCMS <afcm.contact@gmail.com>
2022-03-26 14:38:13 +0100

    Use `mod.conf` if no API call is made

--- init.lua
+++ init.lua
@@ -4,8 +4,14 @@ modname_tooltip = {
 	mods_titles = {},
 }
 
-local function is_mcl(modname)
-	return minetest.get_modpath(modname):match("games/MineClone") --Support MineClone5
+for _,n in ipairs(minetest.get_modnames()) do
+	local modpath = minetest.get_modpath(n)
+	if modpath:match("games/MineClone") then --Support MineClone5
+		modname_tooltip.mods_titles[n] = "MineClone2"
+	else
+		local s = Settings(modpath.."/mod.conf")
+		modname_tooltip.mods_titles[n] = s:get("title") or n
+	end
 end
 
 local function get_modname(itemstring)
@@ -17,12 +23,10 @@ function modname_tooltip.set_mod_title(modname, title)
 end
 
 function modname_tooltip.get_mod_title(modname)
-	if is_mcl(modname) then
-		return "MineClone2"
-	else
-		return modname_tooltip.mods_titles[modname]
-	end
+	return modname_tooltip.mods_titles[modname]
 end
+
+minetest.get_modnames()
 
 tt.register_snippet(function(itemstring)
 	return modname_tooltip.get_mod_title(get_modname(itemstring)), mcl_colors.BLUE
